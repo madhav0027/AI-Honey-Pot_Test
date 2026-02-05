42a923b34a13bd122b9402d89120bad3236044d3
Madhav0027 <ashabobby73@gmail.com>
2026-02-05 14:54:38 +0530

    concatinate conversation history and message

--- src/llm.setup.ts
+++ src/llm.setup.ts
@@ -1,38 +1,49 @@
-import {Groq} from 'groq-sdk'
+import axios from 'axios'
+import { conversation } from './aiagent/conversation';
+import {Groq} from 'groq-sdk';
 
-type ChatMessage = {
-  role: "system" | "user" | "assistant";
-  content: string;
-};
-// Updated runagent with messages parameter
-export async function runagent(messages: ChatMessage[]) {
-  const maxwaits = 8000;
-  const start = Date.now();
-  
-  const APIKEY = process.env.APIKEY;
-  const referer = process.env.APP_URL;
+export async function runagent(systemprompt:string,userprompt:string) {
+    
+    const maxwaits = 8000;
+    const start = Date.now()
 
-  const groq = new Groq({ apiKey: APIKEY });
 
-  while (true) {
-    const res = await groq.chat.completions.create({
-      model: "openai/gpt-oss-120b",
-      temperature: 1,
-      reasoning_effort: "medium",
-      stream: false,
-      messages
-    });
+    const APIKEY = process.env.APIKEY;
+    const referer = process.env.APP_URL;
 
-    const data = res;
-    console.log(data);
-    if (data.choices[0].finish_reason === "stop") {
-      return data.choices[0].message;
+    const groq = new Groq({apiKey:APIKEY});
+    
+    while(true){
+        const res = await groq.chat.completions.create({
+        "messages": [
+            {
+                "role":"system",
+                "content":systemprompt
+            },
+            {
+            "role": "user",
+            "content":userprompt
+            }
+        ],
+        "model": "openai/gpt-oss-120b",
+        "temperature": 1,
+        "stream": false,
+        "reasoning_effort": "medium",
+        });
+
+        const data = res;
+        
+        console.log(data)
+    if(data.choices[0].finish_reason === "stop"){
+        return data.choices[0].message;
+        
     }
 
-    if (Date.now() - start > maxwaits) {
+     if (Date.now() - start > maxwaits) {
       throw new Error("Model failed to load in time");
     }
 
-    await new Promise(r => setTimeout(r, 300));
-  }
+        await new Promise(r => setTimeout(r, 300));
+
+    }
 }
